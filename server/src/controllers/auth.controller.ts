b3db08ac62e60831719064a893ac0156945714d2
charles legault <charleslegault1992@gmail.com>
2026-08-14 23:42:08 -0400

    ajout inscription utilisateur

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -1,12 +1,23 @@
 import type { NextFunction, Request, Response } from "express";
 import { AppError } from "../middlewares/error.js";
-import type { RefreshInput } from "../schemas/auth.schema.js";
+import type { RefreshInput, RegisterInput } from "../schemas/auth.schema.js";
 import { createAccessToken, verifyRefreshToken } from "../utils/jwt.js";
+import { inscrireUtilisateur } from "../services/auth.service.js";
 
-export function register(req: Request, res: Response) {
-  res.status(200).json({
-    message: "Données d'inscription valides",
-  });
+export async function register(
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) {
+  try {
+    const utilisateur = await inscrireUtilisateur(req.body as RegisterInput);
+
+    res.status(201).json({
+      user: utilisateur,
+    });
+  } catch (error) {
+    next(error);
+  }
 }
 
 export function login(req: Request, res: Response) {
--- server/src/services/auth.service.ts
+++ server/src/services/auth.service.ts
@@ -1,4 +1,40 @@
 import prisma from "../utils/prisma.js";
+import { AppError } from "../middlewares/error.js";
+import type { RegisterInput } from "../schemas/auth.schema.js";
+import { hashPassword } from "../utils/password.js";
+
+export async function inscrireUtilisateur(data: RegisterInput) {
+  const utilisateurExistant = await prisma.user.findFirst({
+    where: {
+      OR: [{ email: data.email }, { username: data.pseudonyme }],
+    },
+  });
+
+  if (utilisateurExistant) {
+    throw new AppError(
+      409,
+      "USER_ALREADY_EXISTS",
+      "Ce courriel ou ce pseudonyme est déjà utilisé.",
+    );
+  }
+
+  const motdepasse = await hashPassword(data.password);
+
+  return prisma.user.create({
+    data: {
+      email: data.email,
+      username: data.pseudonyme,
+      motdepasse,
+      role: "UTILISATEUR",
+    },
+    select: {
+      id: true,
+      username: true,
+      email: true,
+      role: true,
+    },
+  });
+}
 
 // sauvegarder l'expiration du token pour le gerer facilement sans dependre du JWT
 export function sauvegarderRefreshToken(
