427d71ff3c90878dc1aa692fcfd0d970077c02a7
eva <20894818+evabessette@users.noreply.github.com>
2026-08-15 18:26:20 -0400

    feat: add main journalEntry row creation loop + activities

--- server/prisma/seed.ts
+++ server/prisma/seed.ts
@@ -1,1 +1,67 @@
+import "dotenv/config";
+
 import { PrismaClient } from "../generated/prisma/client.js";
+import { PrismaNeon } from "@prisma/adapter-neon";
+
+const adapter = new PrismaNeon({ connectionString: process.env.DATABASE_URL });
+const prisma = new PrismaClient({ adapter });
+
+await prisma.journalEntry.deleteMany();
+
+// fonction pour obtenir un chiffre aléatoire entre 1 et 5 inclusif:
+
+function getRandomInt(min: number, max: number): number {
+  const minValue = Math.ceil(min);
+  const maxValue = Math.floor(max);
+  return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
+}
+
+// liste d'évenements pour remplir le field evenements avec un evenement aléatoire:
+
+const evenementsListe = [
+  "Allé à l'épicerie",
+  "Allé au gym",
+  "Téléphoné à ma grand-mère",
+  "Club de course",
+  "Joué avec mon chat",
+  "Joué à DnD avec mes amis",
+  "Marché au parc",
+];
+
+// ajout des activités
+
+await prisma.activity.createMany({
+  data: [
+    { nom: "exercice" },
+    { nom: "travail" },
+    { nom: "méditation" },
+    { nom: "loisirs" },
+    { nom: "thérapie" },
+    { nom: "marche" },
+  ],
+});
+
+// loop pour créer 90 journal entries avec valeurs aléatoires et dates adjacentes:
+
+for (let i: number = 0; i < 90; i += 1) {
+  const d = new Date();
+  d.setDate(d.getDate() - i);
+  await prisma.journalEntry.create({
+    data: {
+      date: d,
+      humeur: getRandomInt(1, 5),
+      energie: getRandomInt(1, 5),
+      qualite_sommeil: getRandomInt(1, 5),
+      anxiete_stress: getRandomInt(1, 5),
+      evenements: evenementsListe[getRandomInt(0, evenementsListe.length - 1)],
+      userId: "cmsttqsje0000i4vuahfp43bh",
+    },
+  });
+}
+
+// test ajout activités
+
+console.log(await prisma.activity.findMany());
+// afficher les journal entries crééés:
+
+// console.log(await prisma.journalEntry.findMany());
